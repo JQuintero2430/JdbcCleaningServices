6733adb5e20a7b426b9e17e0f6b313b883a5dd5a
J Quintero <jfqs24@gmail.com>
2023-09-20 20:51:05 -0500

    se codifica usarioDAOImpl

--- src/main/java/com/cleaningservices/daoImpl/UsuarioDAOImpl.java
+++ src/main/java/com/cleaningservices/daoImpl/UsuarioDAOImpl.java
@@ -1,4 +1,56 @@
 package com.cleaningservices.daoImpl;
 
-public class UsuarioDAOImpl {
+import com.cleaningservices.dao.IDAO;
+import com.cleaningservices.entity.UsuarioEntity;
+import com.cleaningservices.utilities.Constant;
+
+import java.sql.*;
+
+public class UsuarioDAOImpl implements IDAO<UsuarioEntity> {
+
+    private final String URL = Constant.URL;
+    private final String USER = Constant.USER;
+    private final String PASSWORD = Constant.PASSWORD;
+
+    @Override
+    public void insertar(UsuarioEntity usuario) {
+        String queryAddUsuario = "INSERT INTO usuario (NOMBRE, APELLIDO_PATERNO, APELLIDO_MATERNO, " +
+                "EMAIL, NUMERO_TELEFONO, FECHA_NACIMIENTO) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             PreparedStatement statement = connection.prepareStatement(queryAddUsuario)) {
+            statement.setString(1, usuario.getNombreUsuario());
+            statement.setString(2, usuario.getApellidoPaternoUsuario());
+            statement.setString(3, usuario.getApellidoMaternoUsuario());
+            statement.setString(4, usuario.getEmailUsuario());
+            statement.setLong(5, usuario.getTelefonoUsuario());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void mostrarData() {
+        String queryMostrarData = "SELECT * FROM usuario limit 20";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(queryMostrarData)) {
+            System.out.println("ID :  DATOS DEL USUARIO");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getInt("ID_USUARIO") + " : " +
+                        resultSet.getString("NOMBRE") + " " +
+                        resultSet.getString("APELLIDO_PATERNO") + " " +
+                        resultSet.getString("APELLIDO_MATERNO") + " " +
+                        resultSet.getString("EMAIL") + " " +
+                        resultSet.getLong("NUMERO_TELEFONO") + " " +
+                        resultSet.getDate("FECHA_NACIMIENTO"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
