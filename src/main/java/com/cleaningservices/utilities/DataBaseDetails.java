0b906d4f55714bb1f6127ac462c0b4b6dba23d55
J Quintero <99591800+JQuintero2430@users.noreply.github.com>
2023-10-12 07:19:47 -0500

    se cambia DataBaseDetails ce clase a record

--- src/main/java/com/cleaningservices/utilities/DataBaseDetails.java
+++ src/main/java/com/cleaningservices/utilities/DataBaseDetails.java
@@ -1,6 +1,6 @@
 package com.cleaningservices.utilities;
 
-public class DataBaseDetails {
+public record DataBaseDetails() {
     private static final String URL = "jdbc:mysql://localhost:3306/cleaning_database";
     private static final String USER = "root";
     private static final String PASSWORD = "my5q1P455w0rd";
